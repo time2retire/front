3dca6313ed0f4169e7e0f973e02e525810bf6926
David Arias <davidalexarias2@gmail.com>
2018-05-23 20:15:59 -0700

    added chart props and made values more like currency

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -10,15 +10,20 @@ import * as ChartLabels from 'chartjs-plugin-datalabels';
 })
 export class ChartPage {
 
-  x: number = 0;
-  data1: number = 3300 + this.x;
-  data2: any = 2055;
+  dateOfBirth: string;
+  breakEvenYear: number;
+  amtInvested: number;
+  avgIncome: number;
+  fullRetAge: number;
+  retYear: number = 2050;
+  croakYear: number = 2080;
+  retRange: number = this.croakYear - this.retYear;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ngOnInit() {
-    Chart.pluginService.register(ChartLabels);
-  }
+  monthlyBen: number = 1870;
+  totalBen: number = this.monthlyBen * 12 * this.retRange;
+  
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams) {}
   
   public barChartOptions:any = {
     plugins: {
@@ -29,6 +34,10 @@ export class ChartPage {
         font: {
           weight: 'bold',
           size: 25,
+        },
+        formatter: function(value, context) {
+          let currency = (value + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+          return "$" + currency + ".00";
         }
       }
     },
@@ -39,7 +48,7 @@ export class ChartPage {
     },
     scales: {
       yAxes: [{
-        id: 'U.S. Dollars',
+        id: 'A',
           type: 'linear',
           position: 'left',
           ticks: {
@@ -51,13 +60,13 @@ export class ChartPage {
             display: false
           }
         }, {
-        id: 'Years',
+        id: 'B',
           type: 'linear',
           position: 'right',
           ticks: {
-            max: 2060,
-            min: 2020,
-            stepSize: 5
+            max: 1000000,
+            min: 50000,
+            stepSize: 50000
           },
           gridLines: {
             display: false
@@ -74,36 +83,28 @@ export class ChartPage {
   public chartClicked(e:any):void {
     console.log(e);
   }
- 
   public chartHovered(e:any):void {
     console.log(e);
   }
 
-  // public increment(){
-  //   this.x += 50
-  //   this.barChartData[0].data[0]
-  //   console.log(this.barChartData[0].data[0])
-  // }
-
   public barChartData:any[] = [
-    {data: [this.data1], label: 'Monthly Benefit Amt.'},
-    {data: [this.data2], label: 'Break-Even Year'}
+    {data: [this.monthlyBen], label: 'Monthly Benefit Amt.', yAxisID:'A'},
+    {data: [this.totalBen], label: 'Total Benefit', yAxisID: 'B'}
   ];
 
   public randomize():void {
-    this.x += 50;
     let _barChartData:Array<any> = new Array(this.barChartData.length);
     for (let i = 0; i < this.barChartData.length; i++) {
       _barChartData[i] = {data: new Array(this.barChartData[i].data.length), label: this.barChartData[i].label};
       for (let j = 0; j < this.barChartData[0].data.length; j++) {
-        _barChartData[i].data[j] = this.x;
+        _barChartData[i].data[j]
       }
     }
     this.barChartData = _barChartData;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ChartPage');
+    Chart.pluginService.register(ChartLabels);
   }
 
 }
