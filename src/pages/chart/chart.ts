802bb59ae7e3a58712f17d93d640eeccf82d9ea7
Robert Brown <robertlb4@gmail.com>
2018-05-27 12:17:14 -0700

    added slider and ability to update chart based on slider selection

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -19,7 +19,7 @@ export class ChartPage {
   retYear: number = 2050;
   croakYear: number = 2080;
   retRange: number = this.croakYear - this.retYear;
-
+  benefitObject: any;
   monthlyBen: number = 1870;
   totalBen: number = this.monthlyBen * 12 * this.retRange;
   
@@ -95,21 +95,35 @@ export class ChartPage {
     {data: [this.totalBen], label: 'Total Benefit', yAxisID: 'B'}
   ];
 
-  public randomize():void {
-    let _barChartData:Array<any> = new Array(this.barChartData.length);
-    for (let i = 0; i < this.barChartData.length; i++) {
-      _barChartData[i] = {data: new Array(this.barChartData[i].data.length), label: this.barChartData[i].label};
-      for (let j = 0; j < this.barChartData[0].data.length; j++) {
-        _barChartData[i].data[j]
-      }
-    }
-    this.barChartData = _barChartData;
+  // public randomize():void {
+  //   let _barChartData:Array<any> = new Array(this.barChartData.length);
+  //   for (let i = 0; i < this.barChartData.length; i++) {
+  //     _barChartData[i] = {data: new Array(this.barChartData[i].data.length), label: this.barChartData[i].label};
+  //     for (let j = 0; j < this.barChartData[0].data.length; j++) {
+  //       _barChartData[i].data[j]
+  //     }
+  //   }
+  //   this.barChartData = _barChartData;
+  // }
+
+  setChartData(monthlyBen) {
+    let totalBen = monthlyBen * 12 * this.retRange
+    this.barChartData = [
+      {data: [monthlyBen], label: 'Monthly Benefit Amt.', yAxisID:'A'},
+      {data: [totalBen], label: 'Total Benefit', yAxisID: 'B'}
+    ];
+  }
+
+  slideRetAge(event) {
+    this.setChartData(this.benefitObject[event.value].monthlyBen);
   }
 
   ionViewDidLoad() {
     this._api.getRetire()
     .subscribe(data => {
-      console.log(data)
+      this.benefitObject = data;
+      console.log('i ran')
+      this.setChartData(data[63].monthlyBen)
     })
     Chart.pluginService.register(ChartLabels);
   }
