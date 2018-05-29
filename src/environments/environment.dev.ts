70203e0b86e68cf24fd583fbd23e0da1062a62d1
Robert Brown <robertlb4@gmail.com>
2018-05-28 20:05:43 -0700

    edit git ignore for ionic pro

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  mode: 'Development',
+  URL: 'https://time2retire-back.herokuapp.com/'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  mode: 'Production',
+  URL: 'https://time2retire-back.herokuapp.com/'
+}
