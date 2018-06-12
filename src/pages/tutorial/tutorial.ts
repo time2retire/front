3a415bcba723efed9532fc9a88eaf900c9bccc78
Gino17 <galentro451@gmail.com>
2018-06-11 19:10:55 -0700

    added additional template slides with screens of app pages

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -29,6 +29,24 @@ export class TutorialPage {
       "TUTORIAL_SLIDE2_DESCRIPTION",
       "TUTORIAL_SLIDE3_TITLE",
       "TUTORIAL_SLIDE3_DESCRIPTION",
+      "TUTORIAL_SLIDE3_1_TITLE",
+      "TUTORIAL_SLIDE3_1_DESCRIPTION",
+      "TUTORIAL_SLIDE5_TITLE",
+      "TUTORIAL_SLIDE5_DESCRIPTION",
+      "TUTORIAL_SLIDE6_TITLE",
+      "TUTORIAL_SLIDE6_DESCRIPTION",
+      "TUTORIAL_SLIDE7_TITLE",
+      "TUTORIAL_SLIDE7_DESCRIPTION",
+      "TUTORIAL_SLIDE8_TITLE",
+      "TUTORIAL_SLIDE8_DESCRIPTION",
+      "TUTORIAL_SLIDE9_TITLE",
+      "TUTORIAL_SLIDE9_DESCRIPTION",
+      "TUTORIAL_SLIDE10_TITLE",
+      "TUTORIAL_SLIDE10_DESCRIPTION",
+      "TUTORIAL_SLIDE11_TITLE",
+      "TUTORIAL_SLIDE11_DESCRIPTION",
+      "TUTORIAL_SLIDE12_TITLE",
+      "TUTORIAL_SLIDE12_DESCRIPTION",
     ]).subscribe(
       (values) => {
         console.log(ENV)
@@ -42,12 +60,62 @@ export class TutorialPage {
           {
             title: values.TUTORIAL_SLIDE2_TITLE,
             description: values.TUTORIAL_SLIDE2_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-2.png',
+            image: 'assets/img/newImages/Time2Retire 00 Tutorial.png',
           },
           {
             title: values.TUTORIAL_SLIDE3_TITLE,
             description: values.TUTORIAL_SLIDE3_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-3.png',
+            image: 'assets/img/newImages/Time2Retire 01 Welcome 01.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE3_1_TITLE,
+            description: values.TUTORIAL_SLIDE3_1_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 02 Signup.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE5_TITLE,
+            description: values.TUTORIAL_SLIDE5_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 03 Sign in.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE6_TITLE,
+            description: values.TUTORIAL_SLIDE6_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 04 Profile 01.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE7_TITLE,
+            description: values.TUTORIAL_SLIDE7_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 04 Profile 02.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE8_TITLE,
+            description: values.TUTORIAL_SLIDE8_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 04 Profile 03.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE9_TITLE,
+            description: values.TUTORIAL_SLIDE9_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 04 Profile 04.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE10_TITLE,
+            description: values.TUTORIAL_SLIDE10_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 05 Hamburger-Sidemenu.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE11_TITLE,
+            description: values.TUTORIAL_SLIDE11_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 06 Create Chart 01.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE12_TITLE,
+            description: values.TUTORIAL_SLIDE12_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 06 Create Chart 02.png',
+          },
+          {
+            title: values.TUTORIAL_SLIDE13_TITLE,
+            description: values.TUTORIAL_SLIDE13_DESCRIPTION,
+            image: 'assets/img/newImages/Time2Retire 07 Saved Charts 01.png',
           }
         ];
       });
